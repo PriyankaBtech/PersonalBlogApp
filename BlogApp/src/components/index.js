d20954702977caee8790c82f9f7d6c775b357835
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-12-06 12:01:58 +0530

    Added logout-button component

--- BlogApp/src/components/index.js
+++ BlogApp/src/components/index.js
@@ -1,4 +1,5 @@
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
+import Container from './Container/Container'
 
-export { Header, Footer }
+export { Header, Footer, Container }
