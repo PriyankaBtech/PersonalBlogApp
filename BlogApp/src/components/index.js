2193d56e95e324502b0289ddd70c79030f23a41f
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-12-06 12:28:15 +0530

    Added header component

--- BlogApp/src/components/index.js
+++ BlogApp/src/components/index.js
@@ -1,5 +1,13 @@
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
-import Container from './Container/Container'
+import Container from './Container/Container';
+import Logoutbutton from './Header/Logoutbutton';
+import Logo from './Logo';
 
-export { Header, Footer, Container }
+export { 
+    Header,
+    Footer,
+    Container,
+    Logoutbutton,
+    Logo
+}
