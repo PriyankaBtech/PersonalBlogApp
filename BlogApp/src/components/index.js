742d71f1112b8e35f45ff497f063cc96d377b161
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-12-06 12:42:31 +0530

    Added button component

--- BlogApp/src/components/index.js
+++ BlogApp/src/components/index.js
@@ -3,11 +3,13 @@ import Footer from './Footer/Footer';
 import Container from './Container/Container';
 import Logoutbutton from './Header/Logoutbutton';
 import Logo from './Logo';
+import Button from './Button';
 
 export { 
     Header,
     Footer,
     Container,
     Logoutbutton,
-    Logo
+    Logo,
+    Button
 }
