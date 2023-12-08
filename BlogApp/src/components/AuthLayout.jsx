af22a33c1e48b6e9d14fc6278b1e9781b6b45bd3
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-12-08 13:25:24 +0530

    Added auth layout for protection of navigates

---
+++ BlogApp/src/components/AuthLayout.jsx
@@ -0,0 +1,35 @@
+import React, {useEffect, useState} from 'react'
+import {useSelector} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+
+function AuthLayout({children, authentication = true}) {
+
+    const navigate = useNavigate()
+    const [loader, setLoader] = useState(true)
+    const authStatus = useSelector(state => state.auth.status)
+
+    useEffect(() => {
+       // SIMPLE APPROCH 
+
+        // if (authStatus === true){
+        //     navigate("/")
+        // } else if (authStatus === false) {
+        //     navigate("/login")
+        // }
+        
+        //let authValue = authStatus === true ? true : false
+
+        if(authentication && authStatus !== authentication){
+            navigate("/login")
+        } else if(!authentication && authStatus !== authentication){
+            navigate("/")
+        }
+        setLoader(false)
+    }, [authStatus, navigate, authentication])
+
+
+  return  loader ? <h1>Loading...</h1> : <>{children}</>
+
+}
+
+export default AuthLayout
